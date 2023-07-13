cea3f2f75ba100daa70a89f5236e1beaf8f189e0
maw0yemi <139396454+maw0yemi@users.noreply.github.com>
2023-07-13 08:43:07 +0100

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,9 @@
+function toggleNav() {
+    var nav = document.getElementById("myNavbar");
+    if (nav.style.display === "block") {
+      nav.style.display = "none";
+    } else {
+      nav.style.display = "block";
+    }
+  }
+  
