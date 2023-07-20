816d5bc17e5a5d9390a1968133c53927b660d981
maw0yemi <139396454+maw0yemi@users.noreply.github.com>
2023-07-20 17:37:40 +0100

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,9 +1,6 @@
-function toggleNav() {
-    var nav = document.getElementById("myNavbar");
-    if (nav.style.display === "block") {
-      nav.style.display = "none";
-    } else {
-      nav.style.display = "block";
-    }
-  }
-  
+//-----JQUERY TO MANIPULATE THE DOM-----------
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideToggle("slow");
+});
+});
